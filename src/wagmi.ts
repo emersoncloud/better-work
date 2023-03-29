8205384ed653dd7bb7dbff000073116f690bfaa0
Emerson Cloud <emerson.cloud@gmail.com>
2023-03-29 11:50:32 -0400

    add safe connector

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,27 +1,27 @@
-import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy'
+import { configureChains, createClient } from "wagmi";
+import { goerli, mainnet } from "wagmi/chains";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { SafeConnector } from "wagmi/connectors/safe";
+import { WalletConnectLegacyConnector } from "wagmi/connectors/walletConnectLegacy";
 
-import { alchemyProvider } from 'wagmi/providers/alchemy'
+import { alchemyProvider } from "wagmi/providers/alchemy";
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
-  [
-    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! }),
-  ],
-)
+  [mainnet, ...(process.env.NODE_ENV === "development" ? [goerli] : [])],
+  [alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY! })]
+);
 
 export const client = createClient({
   autoConnect: true,
   connectors: [
+    new SafeConnector({ chains }),
     new MetaMaskConnector({ chains }),
     new CoinbaseWalletConnector({
       chains,
       options: {
-        appName: 'wagmi',
+        appName: "wagmi",
       },
     }),
     new WalletConnectLegacyConnector({
@@ -33,11 +33,11 @@ export const client = createClient({
     new InjectedConnector({
       chains,
       options: {
-        name: 'Injected',
+        name: "Injected",
         shimDisconnect: true,
       },
     }),
   ],
   provider,
   webSocketProvider,
-})
+});
